08898dce0ab995176b0e910c9ee6bf53ed4d2b41
Jorenvdp <jorenvdpontseele@gmail.com>
2026-09-24 20:03:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -111,17 +111,16 @@ def normaliseer_naam(naam):
 def kies_seizoen():
     seizoen = request.args.get("seizoen")
     return redirect(url_for("overzicht", seizoen=seizoen))
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-data = laad_data() 
-print("DATA KEYS:", list(data.keys()))
+    data = laad_data() 
+    print("DATA KEYS:", list(data.keys()))
  
-seizoenen = sorted(s for s in data.keys() if s != "spelers")
+    seizoenen = sorted(s for s in data.keys() if s != "spelers")
  
-return render_template(
-"index.html",
-seizoenen=seizoenen
-)
+    return render_template("index.html",seizoenen=seizoenen)
+
 @app.route("/nieuw_seizoen", methods=["POST"])
 def nieuw_seizoen():
     if request.form.get("password") != ADMIN_PASSWORD:
