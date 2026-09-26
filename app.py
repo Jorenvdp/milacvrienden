a0fc0ab52326f10fe0adbe86da55b265bd63df1d
Jorenvdp <jorenvdpontseele@gmail.com>
2026-09-26 18:45:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def wedstrijd(seizoen):
         if request.form.get("password") != ADMIN_PASSWORD:
             return "Geen toegang", 403
 
-        wedstrijdnaam = request.form["wedstrijdnaam"]
+        wedstrijdnaam = request["wedstrijdnaam"]
         tegen = int(request.form["tegen"])
         keeper = request.form["keeper"]
         saves = int(request.form.get("saves", 0))
